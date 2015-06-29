6d5ceb12df19f4d23d9f90315ceb3fca782f378e
Mike Covington <mfcovington@gmail.com>
2015-06-29 11:51:42 -0700

    Pass correct ForeignObject to GenericRelation()

--- data/models.py
+++ data/models.py
@@ -1,12 +1,12 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
-from glue.models import RelatedContent
+from glue.models import DataPublicationRelationship
 
 class Data(models.Model):
     name = models.CharField(max_length=50,)
     description = models.TextField()
-    publication = GenericRelation(RelatedContent, related_query_name='data')
+    publication = GenericRelation(DataPublicationRelationship, related_query_name='data')
 
     def __str__(self):
         return self.name
--- publication/models.py
+++ publication/models.py
@@ -1,12 +1,12 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
-from glue.models import RelatedContent
+from glue.models import DataPublicationRelationship
 
 class Publication(models.Model):
     name = models.CharField(max_length=50,)
     description = models.TextField()
-    data = GenericRelation(RelatedContent, related_query_name='publications')
+    data = GenericRelation(DataPublicationRelationship, related_query_name='publications')
 
     def __str__(self):
         return self.name
