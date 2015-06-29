df275e1b0b357519da3c8a0b00374615e4e504d4
Mike Covington <mfcovington@gmail.com>
2015-06-29 11:42:47 -0700

    Get related data or publications with single function

--- glue/utils.py
+++ glue/utils.py
@@ -19,3 +19,25 @@ def get_related_data(publication_object):
     return [related.data_content_object
         for related in sorted(data_publication_relationships,
             key=lambda x: x.data_content_object.name.lower())]
+
+def get_related_data_publication(object):
+    content_type = ContentType.objects.get_for_model(object)
+    class_name = type(object).__name__.lower()
+    kwargs = {
+        '{}_content_type__pk'.format(class_name): content_type.id,
+        '{}_object_id'.format(class_name): object.id,
+    }
+    data_publication_relationships = DataPublicationRelationship.objects.filter(**kwargs)
+
+    if class_name == 'data':
+        related_class_name = 'publication'
+    elif class_name == 'publication':
+        related_class_name = 'data'
+    # else:
+        # Error
+
+    related_content_object = '{}_content_object'.format(related_class_name)
+
+    return [getattr(related, related_content_object)
+        for related in sorted(data_publication_relationships,
+            key=lambda x: getattr(x, related_content_object).name.lower())]
