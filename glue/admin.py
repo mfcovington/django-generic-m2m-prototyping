849d392e818149da625c84e9ebe76ea340a4d35e
Mike Covington <mfcovington@gmail.com>
2015-06-30 12:09:54 -0700

    Remove generic RelatedContent model

--- glue/admin.py
+++ glue/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from genericadmin.admin import GenericAdminModelAdmin, GenericTabularInline
 
 from .models import (DataPublicationsRelationship, DataScientistsRelationship,
-    PublicationsScientistsRelationship, RelatedContent)
+    PublicationsScientistsRelationship)
 
 
 #######################
@@ -97,13 +97,3 @@ class PublicationsScientistsAdmin(GenericAdminModelAdmin):
 @admin.register(DataScientistsRelationship)
 class DataScientistsAdmin(GenericAdminModelAdmin):
     pass
-class RelatedContentInline(GenericTabularInline):
-    model = RelatedContent
-    ct_field = 'parent_content_type' # See below (1).
-    ct_fk_field = 'parent_object_id' # See below (1).
-
-
-@admin.register(RelatedContent)
-class RelatedContentAdmin(GenericAdminModelAdmin): # Super important! See below (2).
-    content_type_whitelist = ('data/Data', 'publication/Publication' ) # Add white/black lists on this class
-    inlines = [RelatedContentInline,]
--- glue/models.py
+++ glue/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-class RelatedContent(models.Model):
-    """
-    Relates any one entry to another entry irrespective of their individual models.
-    """
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-
-    parent_content_type = models.ForeignKey(ContentType, related_name="parent_test_link")
-    parent_object_id = models.PositiveIntegerField()
-    parent_content_object = GenericForeignKey('parent_content_type', 'parent_object_id')
-
-    def __str__(self):
-        return "{}: {}".format(self.content_type.name, self.content_object)
-
 
 ################
 # BASE CLASSES #
