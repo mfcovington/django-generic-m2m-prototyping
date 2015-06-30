ffe02a8b6f5266c86a28d653417f8b45db1ec64a
Mike Covington <mfcovington@gmail.com>
2015-06-29 19:58:05 -0700

    Compose inline classes from two base classes

--- glue/admin.py
+++ glue/admin.py
@@ -5,18 +5,49 @@ from genericadmin.admin import GenericAdminModelAdmin, GenericTabularInline
 from .models import DataPublicationRelationship, RelatedContent
 
 
-class PublicationToDataRelationshipInline(GenericTabularInline):
-    model = DataPublicationRelationship
+#######################
+# INLINE BASE CLASSES #
+#######################
+
+
+class FromData():
+    ct_field = 'data_content_type'
+    ct_fk_field = 'data_object_id'
+
+
+class FromPublication():
     ct_field = 'publications_content_type'
     ct_fk_field = 'publications_object_id'
+
+
+class ToData():
     ordering = ['data_content_type']
 
 
-class DataToPublicationRelationshipInline(GenericTabularInline):
-    model = DataPublicationRelationship
-    ct_field = 'data_content_type'
-    ct_fk_field = 'data_object_id'
+class ToPublication():
     ordering = ['publications_content_type']
+
+
+############################
+# COMPOSITE INLINE CLASSES #
+############################
+
+
+class DataToPublicationRelationshipInline(FromData, ToPublication,
+    GenericTabularInline):
+
+    model = DataPublicationRelationship
+
+
+class PublicationToDataRelationshipInline(FromPublication, ToData,
+    GenericTabularInline):
+
+    model = DataPublicationRelationship
+
+
+#################
+# ADMIN CLASSES #
+#################
 
 
 @admin.register(DataPublicationRelationship)
