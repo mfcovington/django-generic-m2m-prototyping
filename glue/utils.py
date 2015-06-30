caff71fd4015d53f4a32f475aadd3eab1ea4cdae
Mike Covington <mfcovington@gmail.com>
2015-06-30 12:10:30 -0700

    Remove hard-coded functions to get related content

--- glue/utils.py
+++ glue/utils.py
@@ -2,45 +2,6 @@ from django.contrib.contenttypes.models import ContentType
 
 from .models import DataPublicationsRelationship
 
-def get_related_publications(data_object):
-    data_content_type = ContentType.objects.get_for_model(data_object)
-    data_publication_relationships = DataPublicationsRelationship.objects.filter(
-        data_content_type__pk=data_content_type.id, data_object_id=data_object.id)
-
-    return [related.publication_content_object
-        for related in sorted(data_publication_relationships,
-            key=lambda x: x.publication_content_object.name.lower())]
-
-def get_related_data(publication_object):
-    publication_content_type = ContentType.objects.get_for_model(publication_object)
-    data_publication_relationships = DataPublicationsRelationship.objects.filter(
-        publication_content_type__pk=publication_content_type.id, publication_object_id=publication_object.id)
-
-    return [related.data_content_object
-        for related in sorted(data_publication_relationships,
-            key=lambda x: x.data_content_object.name.lower())]
-
-def get_related_data_publication(object):
-    content_type = ContentType.objects.get_for_model(object)
-    class_name = type(object).__name__.lower()
-    kwargs = {
-        '{}_content_type__pk'.format(class_name): content_type.id,
-        '{}_object_id'.format(class_name): object.id,
-    }
-    data_publication_relationships = DataPublicationsRelationship.objects.filter(**kwargs)
-
-    if class_name == 'data':
-        related_class_name = 'publication'
-    elif class_name == 'publication':
-        related_class_name = 'data'
-    # else:
-        # Error
-
-    related_content_object = '{}_content_object'.format(related_class_name)
-
-    return [getattr(related, related_content_object)
-        for related in sorted(data_publication_relationships,
-            key=lambda x: getattr(x, related_content_object).name.lower())]
 
 def get_related_content(object, object_type, related_type):
     """
