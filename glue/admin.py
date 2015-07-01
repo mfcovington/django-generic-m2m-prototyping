4c4975db3424965ff3dd2ae84e403db5c9e93cbb
Mike Covington <mfcovington@gmail.com>
2015-06-30 17:04:23 -0700

    Generate and register admin classes from a list of relationship tuples

--- glue/admin.py
+++ glue/admin.py
@@ -6,9 +6,9 @@ from .models import (DataPublicationsRelationship, DataScientistsRelationship,
     PublicationsScientistsRelationship)
 
 
-##############################################
-# RELATIONSHIPS FOR GENERATED INLINE CLASSES #
-##############################################
+######################################################
+# RELATIONSHIPS FOR GENERATED ADMIN & INLINE CLASSES #
+######################################################
 
 
 relationships = [
@@ -18,9 +18,9 @@ relationships = [
 ]
 
 
-###########################
-# GENERATE INLINE CLASSES #
-###########################
+###################################
+# ADMIN & INLINE CLASS GENERATORS #
+###################################
 
 
 def generate_tabular_inline_model(relationship):
@@ -39,27 +39,28 @@ def generate_tabular_inline_model(relationship):
     )
     globals()[klass_name] = klass
 
+def generate_and_register_admin_model(relationship):
+    content_1, content_2 = sorted(map(lambda x: x.capitalize(), relationship))
+    klass_name = ''.format(content_1, content_2)
+    klass = type(
+        klass_name,
+        (GenericAdminModelAdmin,),
+        {
+            '__module__': __name__,
+        }
+    )
+    globals()[klass_name] = klass
+
+    model = eval('{}{}Relationship'.format(content_1, content_2))
+    admin.site.register(model, klass_name)
+
+
+####################
+# GENERATE CLASSES #
+####################
+
 
 for r in relationships:
     generate_tabular_inline_model(sorted(r))
     generate_tabular_inline_model(sorted(r, reverse=True))
-
-
-#################
-# ADMIN CLASSES #
-#################
-
-
-@admin.register(DataPublicationsRelationship)
-class DataPublicationsAdmin(GenericAdminModelAdmin):
-    pass
-
-
-@admin.register(PublicationsScientistsRelationship)
-class PublicationsScientistsAdmin(GenericAdminModelAdmin):
-    pass
-
-
-@admin.register(DataScientistsRelationship)
-class DataScientistsAdmin(GenericAdminModelAdmin):
-    pass
+    generate_and_register_admin_model(r)
